e12ccfd7dbbbf5459708f5c8fbf6964b1cc4481b
sami5671 <112261102+sami5671@users.noreply.github.com>
2025-07-19 23:18:28 +0600

    all models logic with relationship done

--- 7. ECommerce project/shop/models.py
+++ 7. ECommerce project/shop/models.py
@@ -58,3 +58,75 @@ class Rating(models.Model):
 
     def __str__(self):
         return f"{self.user.username}- {self.product.name} - {self.rating}"
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Cart For{self.user.username}"
+
+    def get_total_price(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+    def get_total_item(self):
+        return sum(item.quantity for item in self.items.all())
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} X {self.product.name}"
+
+    def get_cost(self):
+        return self.product.price * self.quantity
+
+
+class Order(models.Model):
+    STATUS_CHOICES = (
+        ("pending", "Pending"),
+        ("processing", "Processing"),
+        ("shipped", "Shipped"),
+        ("delivered", "Delivered"),
+        ("canceled", "Canceled"),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="pending")
+    note = models.TextField()
+    paid = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=200, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created"]
+
+    def __str__(self):
+        return f"Order #{self.id}"
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} X {self.product.name}"
+
+    def get_cost(self):
+        return self.price * self.quantity
