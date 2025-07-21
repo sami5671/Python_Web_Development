f4e0668ce124792d49ccc817d28072fe54a7fa57
sami5671 <112261102+sami5671@users.noreply.github.com>
2025-07-21 12:20:01 +0600

    login, signup, signup email varification functionality done

---
+++ 8. FastKart_Ecommerce/accounts/views.py
@@ -0,0 +1,66 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.tokens import default_token_generator
+from django.shortcuts import redirect, render
+from django.utils.http import urlsafe_base64_decode
+
+from accounts.forms import CustomUserRegistrationForm
+from accounts.utils import send_verification_email
+
+from .models import CustomUser
+
+
+def user_signup(request):
+    if request.method == "POST":
+        form = CustomUserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            send_verification_email(request, user)
+            messages.info(request, "We have sent you an verification email")
+            return redirect("login")
+        else:
+            return redirect("signup")
+    else:
+        return render(request, "accounts/signup.html")
+
+
+def verify_email(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = CustomUser.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, CustomUser.DoesNotExist):
+        user = None
+
+    if user and default_token_generator.check_token(user, token):
+        user.is_verified = True
+        user.save()
+        messages.success(request, "Your email has been verified successfully.")
+        return redirect("login")
+    else:
+        messages.error(request, "The verification link is invalid or has expired.")
+        return redirect("signup")
+
+
+def user_login(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        user = authenticate(request, email=email, password=password)
+        if not user:
+            messages.error(request, "Invalid username or password.")
+        elif not user.is_verified:
+            messages.error(request, "Your email is not verified yet.")
+        else:
+            login(request, user)
+            messages.success(request, "You have successfully logged in.")
+            return redirect("profile")
+
+    # TODO: use a form and show form errors in template
+    return render(request, "accounts/login.html")
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect("signup")
