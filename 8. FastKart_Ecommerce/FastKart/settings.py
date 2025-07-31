0d3ff6801bc50dc4de6eea2fe1236084640ff800
sami5671 <112261102+sami5671@users.noreply.github.com>
2025-07-31 18:32:07 +0600

    email varification done

--- 8. FastKart_Ecommerce/FastKart/settings.py
+++ 8. FastKart_Ecommerce/FastKart/settings.py
@@ -144,10 +144,12 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "samialam5671@gmail.com"
-EMAIL_HOST_PASSWORD = "mhrr mjkc nytc vbbf"
-# EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-# EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+
+# EMAIL_HOST_USER = "samialam5671@gmail.com"
+# EMAIL_HOST_PASSWORD = "mhrr mjkc nytc vbbf"
+
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
